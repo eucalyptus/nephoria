8440cab26090caad5e1662746c62eb9f28e37f8e
Matt Clark <mclark@eucalyptus.com>
2015-05-12 13:09:10 -0700

    Clean up sample test, and readme

--- eutester/testcases/sample_testsuite.py
+++ eutester/testcases/sample_testsuite.py
@@ -1,32 +1,38 @@
 from eutester.testcase_utils.eutestcase import EutesterTestCase, SkipTestException
 from cloud_utils.net_utils.sshconnection import SshConnection
 
-# Create the testcase object
+##################################################################################################
+# Create the testcase object                                                                     #
+##################################################################################################
 testcase = EutesterTestCase()
 
-
-# See 'setup_parser' for all the default cli arguments. These can be negated by setting
-# these to 'false' as shown here...
+##################################################################################################
+# See 'setup_parser' for all the default cli arguments. These can be negated by setting          #
+# these to 'false' as shown here...                                                              #
+##################################################################################################
 testcase.setup_parser(testname='My first test',
                       description='Sample testcase',
                       emi=False, # exclude the emi cli option from this test
                       testlist=False # Exclude the 'testlist' cli option for this test
                       )
 
-
-# Add an additional CLI argument to this test
+##################################################################################################
+# Add an additional CLI argument to this test                                                    #
+##################################################################################################
 testcase.parser.add_argument('--test_ip',
                              help='IP address used to create SSH connection sample test',
                              default=None)
 
-
-# Gather CLI arguments, this will also parse any arguments in any config files
-# that are provided. The cli args and config file args will be merged into testcase.args
+##################################################################################################
+# Gather CLI arguments, this will also parse any arguments in any config files                   #
+# that are provided. The cli args and config file args will be merged into testcase.args         #
+##################################################################################################
 testcase.get_args()
 
-
-# Add a test method, Name is not important to running, but does help organize the suite.
-# Methods will later be used/created when converted to testunit class objs shown later(at bottom)
+##################################################################################################
+# Add a test method, Name is not important to running, but does help organize the suite.         #
+# Methods will later be used/created when converted to testunit class objs shown later(at bottom)#
+##################################################################################################
 def my_first_test_method(ip=None, username=None, password=None):
     """
     Description: This description will be displayed in the test run output and should
@@ -47,8 +53,8 @@ def my_first_test_method(ip=None, username=None, password=None):
     ssh = SshConnection(host=ip, username=username, password=password, timeout=10, retry=0,
                         verbose=True)
 
-    # SshConnection.sys() will execute a cmd on the remote system, 'code' will check the return code of the
-    # command and throw an exception if it does not match the value provided.
+    # SshConnection.sys() will execute a cmd on the remote system, 'code' will check the return
+    # code of the command and throw an exception if it does not match the value provided.
     # By default the returned output is a list of lines, but can also be returned as a single
     # string buffer using 'listformat=False')
     # The command is monitored and the session will be torn down and a timeout exception will be
@@ -61,13 +67,19 @@ def my_first_test_method(ip=None, username=None, password=None):
     testcase.debug('Heres the output of our two commands:\n{0}'.format(output))
 
 
-# Now add a method that is intended to fail...
+##################################################################################################
+# Now add a method that is intended to fail...                                                   #
+##################################################################################################
 def sample_test_fail_method():
     return my_first_test_method(ip=None, username='noone', password='badpassword')
 
 testcase.uh_oh_fail = sample_test_fail_method
 testcase.uh_oh_fail.__doc__ = "Description: This test should demonstrate a failure...\n"
 
+
+##################################################################################################
+# Add a test which demonstrates how to skip a test from within the test method                   #
+##################################################################################################
 def too_lazy_to_run():
     """
     This shows how to throw a 'SkipTestException' in the case a test detects it should not be
@@ -77,6 +89,10 @@ def too_lazy_to_run():
     """
     raise SkipTestException('Im too lazy to run right now')
 
+
+##################################################################################################
+# Add a test which will later demonstrate how the eof (end on failure) flag works                #
+##################################################################################################
 def hope_i_get_to_run():
     '''
     'This test should show a failure of a test which never gets to run'
@@ -85,20 +101,32 @@ def hope_i_get_to_run():
     testcase.debug('This test should show a failure of a test which never gets to run'
                    'due to EOF(End On Failure) set for a failed testcase')
 
-# Create our test list of test units...
-# this can be done by using a method or the name of a method within the testcase class...
-# by passing a method
+##################################################################################################
+# Create our test list of test units...                                                          #
+# this can be done by using a method or the name of a method within the testcase class...        #
+# by passing a method                                                                            #
+##################################################################################################
 test1 = testcase.create_testunit_from_method(my_first_test_method)
 test2 = testcase.create_testunit_from_method(too_lazy_to_run)
-# By passing a name of a method local to the testcase object, setting eof to True here will
-# abort any remaining tests if this unit fails. This can also be set globally for all test units
-# during run_test_case_list()
+
+##################################################################################################
+# By passing a name of a method local to the testcase object, setting eof to True here will      #
+# abort any remaining tests if this unit fails. This can also be set globally for all test units #
+# during run_test_case_list()                                                                    #
+##################################################################################################
 test3 = testcase.create_testunit_by_name('uh_oh_fail', eof=True)
 test4 = testcase.create_testunit_from_method(hope_i_get_to_run)
 
+
+##################################################################################################
+# Finally run the test list                                                                      #
+##################################################################################################
 result = testcase.run_test_case_list(list = [test1, test2, test3, test4],
                                      eof=False,
                                      clean_on_exit=False,
                                      printresults=True)
 
+##################################################################################################
+# Dont forget to exit with the proper code                                                       #
+##################################################################################################
 exit(result)
