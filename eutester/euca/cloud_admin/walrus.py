c93cffc8fece8cf7973a057c4436c18ff327208f
Matt Clark <mclark@eucalyptus.com>
2015-05-11 17:36:41 -0700

    Add individual componet classes

---
+++ eutester/euca/cloud_admin/walrus.py
@@ -0,0 +1,14 @@
+__author__ = 'clarkmatthew'
+
+
+from eutester.euca.cloud_admin.services import EucaComponentService, SHOW_COMPONENTS
+
+
+class EucaWalrusBackendService(EucaComponentService):
+
+    def update(self, new_service=None, get_instances=True, silent=True):
+        return self._update(get_method_name='get_walrus_backend_service',
+                            get_method_kwargs=None, new_service=new_service, silent=silent)
+
+    def show(self):
+        return SHOW_COMPONENTS(self.connection, self)
