d3ba2b8cf84ba54d932fcc48b464f125798c0b27
Matt Clark <mclark@eucalyptus.com>
2016-02-11 12:26:20 -0800

    Add simple bash with git install script

---
+++ toolbox/install_script_git.sh.py
@@ -0,0 +1,25 @@
+#!/bin/bash
+venv="nephoria_venv"
+neph_branch="oldboto"
+adminapi_branch="master"
+yum install -y python-devel gcc git python-setuptools python-virtualenv
+if [ ! -d adminapi ]; then
+    git clone https://github.com/nephomaniac/adminapi.git
+fi
+if [ ! -d nephoria ]; then
+    git clone https://github.com/nephomaniac/nephoria.git
+fi
+if [ "x$venv" != "x" ]; then
+    if [ ! -d $venv ]; then
+        virtualenv $venv
+    fi
+    source $venv/bin/activate
+fi
+cd adminapi
+git checkout $adminapi_branch
+python setup.py install
+cd -
+cd nephoria
+git checkout $neph_branch
+python setup.py install
+cd -
