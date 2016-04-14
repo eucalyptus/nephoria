2dd23a2c9eb42e89e0276e4bf0e49618831a89e2
Matt Clark <mclark@eucalyptus.com>
2016-04-14 09:59:23 -0700

    More docs, readme and testcase suite example

---
+++ nephoria/testcases/README.md
@@ -0,0 +1,9 @@
+For examples/tutorials on writing and running test units please see:
+nephoria/testcases/sample_test_suite.py
+
+For additional examples, usage, please see the following base templates:
+nephoria/testcases/boto/ec2/base_ec2_template.py
+nephoria/testcases/boto/ec2/instances/run_instances.py
+
+For basic example of a legacy eutester test which has been converted to nephoria please see:
+nephoria/testcases/boto/ec2/network/net_tests_classic.py
--- nephoria/testcases/sample_test_suite.py
+++ nephoria/testcases/sample_test_suite.py
@@ -3,7 +3,6 @@ from nephoria.testcase_utils.cli_test_runner import CliTestRunner, SkipTestExcep
 import copy
 import time
 
-
 """
 This is intended to demonstrate some basic ways to write a test suite.
 
@@ -32,6 +31,20 @@ In [4]: test.test1()
 
 """
 
+##################################################################################################
+# Extend the CliTestRunner Class to create a new test suite
+# - The '_DEFAULT_CLI_ARGS' attribute defines the cli arguments available to the user at run time.
+#   See the CliTestRunner base class for all the default cli arguments
+#   The following example shows how to add CLI arguments to this class.
+#   From a command prompt/shell the default CLI arguments can also be seen by typing:
+#   Command Line Example:
+#       'python CliTestRunner.py --help'
+# - Any argument defined in '_DEFAULT_CLI_ARGS' can also be defined during CliTestRunner.__init__()
+#   if objects of type CliTestRunner are to be used within other python modules/scripts.
+#   Embedded Python Example:
+#       from nephoria.testcase_utils.cli_test_runner import CliTestRunner
+#       mytestcaseobject = CliTestRunner(clc='1.2.3.4', password='mypass', log_level='debug')
+##################################################################################################
 
 class SampleTestSuite1(CliTestRunner):
     _DEFAULT_CLI_ARGS = copy.copy(CliTestRunner._DEFAULT_CLI_ARGS)
@@ -40,12 +53,24 @@ class SampleTestSuite1(CliTestRunner):
                                                   'default': None,
                                                   'required': True}}
 
+    ###############################################################################################
+    # A 'test' method can be named anything, but if not prefixed with the word 'test' the testcase
+    # author will need to feed the methods into the runner explicitly.
+    # By default the CliTestRunner.run() method will execute all local methods prefixed with the
+    # word 'test' in sorted order of the name (ie test1_blah, test2_yada, testa_blah,...)
+    # Any 'test' method should be capable of running autonomously. If the test has dependencies,
+    # it should be able to re-use or create the artifacts it needs, or fail with an error explaining
+    # that it can or will not do so, and what the user needs to do prior to running this test.
+    ###############################################################################################
+
     def test1(self, string_to_print='This is the default'):
         self.log.info(string_to_print)
 
     def test2_ditto(self):
         self.test1(string_to_print='This is test2 now')
 
+    # Tests can raise the 'SkipTestException' to skip a test w/o failure. If a test detects the
+    # it is not intended for the test environment it should raise this to skip itself.
     def test12_skip_me(self):
         raise SkipTestException('I was too lazy to run')
 
@@ -54,6 +79,7 @@ class SampleTestSuite1(CliTestRunner):
         if a == 1:
             raise ValueError('a == 1, so I am failing')
 
+    # Test can reference the command line arguments using the 'args' attribute.
     def test3_arg_usage(self):
         print "Heres all the command line arguments in self._DEFAULT_CLI_ARGS..."
         self.show_args()
@@ -63,8 +89,36 @@ class SampleTestSuite1(CliTestRunner):
         else:
             self.log.info('You set the sample arg to: "{0}"'.format(self.args.sample_arg))
 
+    ###############################################################################################
+    # At the end of each full testcase run, the clean_method() is called unless 'no_clean' arg
+    # is set to True. Each test case will need to implement this method to enforce tests do not
+    # leave test artifacts behind on a system, resource leaks, etc.. Tests that do not leave
+    # the system in the state it found it should be clear about the test's intention and any
+    # resulting artifacts intended to remain after the test has ran.
+    ###############################################################################################
+
     def clean_method(self):
         print 'Cleaning...'
+
+##################################################################################################
+# Define main() for when this module is invoked from the command line
+# here the module creates an object of itself, invokes run() and exits with the proper return
+# code
+# During run():
+# - function/methods intended to be run as 'tests' are converted to
+#   cli_test_runner.TestUnit() type objects. These test unit object carry their results, metrics,
+#   loggers, run time arguments/attributes, etc..
+# - While the test units are being executed information about the test is logged to stdout and/or
+#   a defined log file. (see the CliTestRunner default args for logger settings).
+# - When a testunit is first started, an introduction header is logged. The information in this
+#   header is contained in the  TestUnit.test_unit_description, and defaults to a method/function's
+#   doc-string if not provided.
+# - At the end of each TestUnit the result of the testunit is logged as well as a brief summary
+#   of all the testunit results. When the entire suite has completed (at the end of run()),
+#   a summary of all the tests run, their results, time to complete, as well as brief descriptions
+#   of any errors/failures is logged in table format.
+# - It is up to the testcase author to exit with the proper return code. (see below)
+##################################################################################################
 
 if __name__ == "__main__":
 
