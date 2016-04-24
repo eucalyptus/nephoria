614dfbff6b23c874c93b2fd42d9779fa24531e12
Imran Hossain Shaon <mdshaonimran@gmail.com>
2016-04-24 13:29:19 -0700

    add env param in nettest

--- nephoria/testcases/boto/ec2/network/net_tests_classic.py
+++ nephoria/testcases/boto/ec2/network/net_tests_classic.py
@@ -163,12 +163,13 @@ class NetTestsClassic(CliTestRunner):
         tc = getattr(self, '_test_controller', None)
         if not tc:
             clc_ip = self.args.clc
+            environment_file = self.args.environment_file
             clc_pw = self.args.password
             test_account = self.args.test_account
             test_user = self.args.test_user
             log_level = getattr(self.args, 'log_level', 'DEBUG')
-            tc = TestController(hostname=clc_ip, password=clc_pw, log_level=log_level,
-                                clouduser_name=test_user, clouduser_account=test_account)
+            tc = TestController(hostname=clc_ip, environment_file=environment_file, password=clc_pw,
+                                log_level=log_level, clouduser_name=test_user, clouduser_account=test_account)
             setattr(self, '_test_controller', tc)
         return tc
 
