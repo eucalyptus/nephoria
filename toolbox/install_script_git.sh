10478341f6b70de931f92e45058b9564c4ef21d3
root <root@b-39.qa1.eucalyptus-systems.com>
2016-05-16 17:42:14 -0700

    Update install script

--- toolbox/install_script_git.sh
+++ toolbox/install_script_git.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -x
 venv="nephoria_venv"
-neph_branch="oldboto"
+neph_branch="master"
 adminapi_branch="master"
-yum install -y python-devel gcc git python-setuptools python-virtualenv libffi-devel openssl-devel
+yum install -y python-devel gcc git python-setuptools python-virtualenv
 rpm -qa | grep virtualenv # verify it was installed successfully above
 yum repolist # check repos
 if [ ! -d adminapi ]; then
-    git clone https://github.com/nephomaniac/adminapi.git
+    git clone https://github.com/eucalyptus/adminapi.git
 fi
 if [ ! -d nephoria ]; then
-    git clone https://github.com/nephomaniac/nephoria.git
+    git clone https://github.com/eucalyptus/nephoria.git 
 fi
 if [ "x$venv" != "x" ]; then
     if [ ! -d $venv ]; then
