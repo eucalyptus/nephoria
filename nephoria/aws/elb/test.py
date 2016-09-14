76d2933c4652c23253a863fec5a172bfbee8b180
aliceh <alice@eucalyptus.com>
2016-09-14 14:37:00 -0700

    deleted test file

--- nephoria/aws/elb/test.py
+++
@@ -1,9 +0,0 @@
-from nephoria.testcontroller import TestController
-tc = TestController(hostname="10.111.1.73", log_level='DEBUG', password='foobar')
-user=tc.create_user_using_cloudadmin('ui-test-acct-03','admin')
-
-print""
-print "user.cloudformation.delete_all_stacks()"
-print""
-stacks=user.cloudformation.delete_all_stacks()
-
